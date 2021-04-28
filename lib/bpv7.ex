87d64f8c6e25c1d46a3f90169ce10c95f1772fd0
Leonard Wagner <leonard.wagner@mailbox.tu-dresden.de>
2021-04-28 14:48:50 +0200

    Initial Commit

---
+++ lib/bpv7.ex
@@ -0,0 +1,18 @@
+defmodule Bpv7 do
+  @moduledoc """
+  Documentation for `Bpv7`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bpv7.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bpv7_test.exs
@@ -0,0 +1,8 @@
+defmodule Bpv7Test do
+  use ExUnit.Case
+  doctest Bpv7
+
+  test "greets the world" do
+    assert Bpv7.hello() == :world
+  end
+end
