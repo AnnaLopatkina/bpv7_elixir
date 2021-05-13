04ef55cdf0694f4ff9c0adf28b132c513f402e8b
Leonard Wagner <leonard.wagner@mailbox.tu-dresden.de>
2021-05-13 15:02:07 +0200

    Added CBOR implementation

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Bpv7.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:cbor, git: "https://github.com/galdor/erl-cbor.git"}
+      {:cbor, "~> 1.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
