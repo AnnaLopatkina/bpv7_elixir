45053d539558cb2e65f90352fb36ca5046b5a39a
Leonard Wagner <leonard.wagner@mailbox.tu-dresden.de>
2021-05-19 19:44:31 +0200

    Fixed typing error

--- lib/canonical__block.ex
+++ lib/canonical__block.ex
@@ -7,7 +7,7 @@ defmodule Canonical_Block do
     field :block_number, integer()
     field :block_control_flags, Block_Control_Flags.t()
     field :crc_type, CRC_Type.t()
-    field :crc, list(bytes())
+    field :crc, list(byte())
     field :value, Extension_Block.t()
   end
 
