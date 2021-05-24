873cbb55f99ec943907292a343855cfdba93d5a5
Leonard Wagner <leo.wagner1401@web.de>
2021-05-24 22:51:05 +0200

    Bpv7 epoch calculation milliseconds for timestamp

--- lib/bpv7.ex
+++ lib/bpv7.ex
@@ -22,12 +22,18 @@ defmodule Bpv7 do
     bundle_control_flags = %Bundle_Control_Flags{bundle_must_not_be_fragmented: true}
     endpointid = %EndpointID{scheme_name: "dtn", scheme_number: 1, authority: "u3dtn-node", path: "node1", is_singleton: true}
     endpointidsource = %EndpointID{scheme_name: "dtn", scheme_number: 1, authority: "u3dtn-node", path: "node2", is_singleton: true}
-    creationtimestamp = %Creation_Time_Stamp{milliseconds: 5, sequence: 1}
+
+    bpv7_epoch = "2000-01-01 00:00:00"
+    bpv7_epoch_date = Timex.parse!(bpv7_epoch, "%Y-%m-%d %H:%M:%S", :strftime)
+    bpv7_epoch_milliseconds = DateTime.to_unix(Timex.to_datetime(bpv7_epoch_date), :milliseconds)
+    now_milliseconds = DateTime.to_unix(Timex.now(), :milliseconds)
+    creationtimestamp = %Creation_Time_Stamp{milliseconds: now_milliseconds-bpv7_epoch_milliseconds, sequence: 1}
 
     primaryblock = %Primary_Block{version: 7, bundle_control_flags: bundle_control_flags, crc_type: 1, \
     destination: endpointid, source_node: endpointidsource, report_to: endpointid, creation_time_stamp: creationtimestamp, \
     lifetime: 360000, crc: [0000,0000,0000,0000]}
     primaryblock
+    now_milliseconds
 
   end
 end
