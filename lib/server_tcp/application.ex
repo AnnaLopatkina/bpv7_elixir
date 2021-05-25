47042b4cf8052dbc2844e5c3aae2acd679f6ee08
AnnaLopatkina <lopaat1009@gmail.com>
2021-05-25 23:24:05 +0200

    refactor the structure

--- lib/server_tcp/application.ex
+++ lib/server_tcp/application.ex
@@ -2,10 +2,10 @@ defmodule Application do
   @moduledoc false
   def start(_type, _args) do
     children = [
-      {Task, fn -> KVServer.accept(4040) end}
+      {Task, fn -> Server.accept(4040) end}
     ]
 
-    opts = [strategy: :one_for_one, name: KVServer.Supervisor]
+    opts = [strategy: :one_for_one, name: Server.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
