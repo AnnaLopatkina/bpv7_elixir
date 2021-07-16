74793e42a0eb15ab52c0c47c681c0ed8aead5e72
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2021-07-17 01:21:28 +0200

    implement scheduling mechanism

--- lib/application.ex
+++ lib/application.ex
@@ -6,13 +6,14 @@ defmodule Bpv7.Application do
   def start(_type, _args) do
     children = [
       {Task.Supervisor, name: Bpv7.Server.TaskSupervisor},
-      {Task, fn -> Bpv7.Server.accept(4040) end}
+      {Task, fn -> Bpv7.Server.accept(4040) end},
+      Bpv7.BPA
     ]
 
     opts = [strategy: :one_for_one, name: Bpv7.Server.Supervisor]
     Supervisor.start_link(children, opts)
 
     Bpv7.ConnManager.start([])
-    Bpv7.BPA.start([])
+
   end
 end
--- lib/bpa.ex
+++ lib/bpa.ex
@@ -1,16 +1,20 @@
 defmodule Bpv7.BPA do
   use Agent
+  use GenServer
 
   @doc """
   Starts a new Instance of the Bundle Protocol Agent (BPA)
 
   The BPA has an Agent which holds a map with the eid as key and a tuple {protocol, host, port, avail_begin, avail_end}.
   """
-  def start(_opts) do
-    node_name = :BPA_nodes
-    bundle_name =  :BPA_bundles
+  def start_link(_opts) do
     Agent.start_link(fn -> %{} end, name: :nodes)
     Agent.start_link(fn -> %{} end, name: :bundles)
+    GenServer.start_link(__MODULE__, %{}, [name: __MODULE__])
+  end
+
+  def init(state) do
+    {:ok, state}
   end
 
   def add_tcp_node(eid, host, port, avail_begin, avail_end) do
@@ -30,6 +34,32 @@ defmodule Bpv7.BPA do
   def get_availability(eid) do
     {_, _, _, avail_begin, avail_end} = Agent.get(:nodes, &Map.get(&1, eid))
     {avail_begin, avail_end}
+  end
+
+  def schedule_bundle(bundle, eid) do
+    {avail_begin, avail_end} = get_availability(eid)
+    current_time = DateTime.utc_now()
+    schedule_time = DateTime.diff(avail_begin, current_time)
+    schedule_time = cond do
+      schedule_time < 0 ->
+        0
+      true ->
+        schedule_time
+    end
+    Process.send_after(__MODULE__,{:send, bundle, eid}, schedule_time)
+    :ok
+  end
+
+  def handle_info({:send, bundle, eid}, state) do
+    :tcp = get_connection_method(eid)
+    {host, port} = get_tcp_conn_details(eid)
+    connState = Bpv7.ConnManager.check_connection(host, port)
+    case connState do
+      :not_found -> :ok = Bpv7.ConnManager.connect(host, port)
+      :ok -> nil
+    end
+    Bpv7.ConnManager.send(host, port, bundle)
+    {:noreply, state}
   end
 
 end
