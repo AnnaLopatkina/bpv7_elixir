9f10b3f177bc9165d6d99ecc0d06cce28e56bd93
Leonard Wagner <leo.wagner1401@web.de>
2021-07-19 12:35:20 +0200

    assert_raise with error message

--- test/bundlemanager_test.exs
+++ test/bundlemanager_test.exs
@@ -12,7 +12,7 @@ defmodule Bundlemanager_test do
     assert Bpv7.Bundle_Manager.check_bundle_crc(bundle_array) == :ok
 
     bundle_array_false = Bpv7.Bundle_Manager.decode_cbor_bundle("9f890700028201722f2f632e64746e2f62756e646c6573696e6b8201672f2f612e64746e820100821b0000009e3a0b75cf031a000493e044b201c5f4860a0200024482181e004487d25ff88607030002410044a0a52ecd86010100024c48656c6c6f20576f726c6421447585c26eff")
-    assert_raise RuntimeError, Bpv7.Bundle_Manager.check_bundle_crc(bundle_array_false)
+    assert_raise RuntimeError, "canonical checksums not correct", Bpv7.Bundle_Manager.check_bundle_crc(bundle_array_false)
   end
 
 end
