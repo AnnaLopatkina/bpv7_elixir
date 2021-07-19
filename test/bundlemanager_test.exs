b3afedf7f3d7ef419702afd23507107f4409affe
Leonard Wagner <leo.wagner1401@web.de>
2021-07-19 13:05:44 +0200

    undifined function fix

--- test/bundlemanager_test.exs
+++ test/bundlemanager_test.exs
@@ -24,8 +24,8 @@ defmodule Bundlemanager_test do
 
   test "hop_count_block" do
     bundle_array = Bpv7.Bundle_Manager.decode_cbor_bundle("9f890700028201722f2f632e64746e2f62756e646c6573696e6b8201672f2f612e64746e820100821b0000009e3a0b75cf031a000493e044b201c5f4860a0200024482181e004487d25ff88607030002410044a0a52ecd86010100024c48656c6c6f20576f726c6421447585c26dff")
-    hopCountBlock = get_canonical(bundle_array, 1)
-    hopCountBlockArray = canonical_to_array(hopCountBlock)
+    hopCountBlock = Bpv7.BundleManager.get_canonical(bundle_array, 1)
+    hopCountBlockArray = Bpv7.BundleManager.canonical_to_array(hopCountBlock)
 
    {:ok, specific_data_encoded} = Map.fetch(Enum.at(hopCountBlockArray, 4), :value)
    {:ok, specific_data_decoded, ""} = CBOR.decode(specific_data_encoded)
@@ -33,8 +33,8 @@ defmodule Bundlemanager_test do
     hop_count = Enum.at(specific_data_decoded, 1)
 
     bundle_array_updated = Bpv7.Bundle_Manager.change_Hop_Count_Block(bundle_array)
-    hopCountBlock = get_canonical(bundle_array_updated, 1)
-    hopCountBlockArray = canonical_to_array(hopCountBlock)
+    hopCountBlock = Bpv7.BundleManager.get_canonical(bundle_array_updated, 1)
+    hopCountBlockArray = Bpv7.BundleManager.canonical_to_array(hopCountBlock)
 
     {:ok, specific_data_encoded} = Map.fetch(Enum.at(hopCountBlockArray, 4), :value)
     {:ok, specific_data_decoded, ""} = CBOR.decode(specific_data_encoded)
