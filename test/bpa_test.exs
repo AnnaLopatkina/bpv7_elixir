59c29ea18a11fa6f64b63feb3de323b2b7a10c1a
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2021-07-27 17:58:29 +0200

    adjust BPA testcase for entry removal

--- test/bpa_test.exs
+++ test/bpa_test.exs
@@ -6,11 +6,24 @@ defmodule Bpv7.BPA_test do
 
     {:ok, begin_time, 0} = DateTime.from_iso8601("2021-07-17T13:05:00Z")
     {:ok, end_time, 0} = DateTime.from_iso8601("2021-07-19T23:48:00Z")
-    Bpv7.BPA.add_tcp_node("dtn://test.dtn",'localhost', 4040, begin_time, end_time)
+    current_time = DateTime.utc_now()
+    end_time = DateTime.add(current_time, 3600, :second)
+    Bpv7.BPA.add_tcp_node("dtn://test.dtn",'localhost', 4040, current_time, end_time)
 
     assert Bpv7.BPA.get_connection_method("dtn://test.dtn") == :tcp
     assert Bpv7.BPA.get_tcp_conn_details("dtn://test.dtn") == {'localhost', 4040}
-    assert Bpv7.BPA.get_availability("dtn://test.dtn") == {begin_time, end_time}
+    assert Bpv7.BPA.get_availability("dtn://test.dtn") == {current_time, end_time}
 
+  end
+
+  test "remvove outdated entries" do
+    assert Bpv7.BPA.get_tcp_conn_details("dtn://test-remove.dtn") == :not_found
+    
+    current_time = DateTime.utc_now()
+    end_time = DateTime.add(current_time, -5, :second)
+    current_time |> DateTime.add(-10, :second)
+    Bpv7.BPA.add_tcp_node("dtn://test-remove.dtn",'localhost', 4040, current_time, end_time)
+
+    assert Bpv7.BPA.get_tcp_conn_details("dtn://test-remove.dtn") == :not_found
   end
 end
